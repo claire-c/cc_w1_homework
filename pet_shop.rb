f54fe01b2b80dacaf1f9ed02ac0f569439627c5f
Claire <cconnachan@gmail.com>
2018-03-11 17:11:35 +0000

    refactored

--- pet_shop.rb
+++ pet_shop.rb
@@ -24,19 +24,19 @@ end
 
 def pets_by_breed(shop, breed_search)
   breed_total = []
-    for each_pet in shop[:pets]
+  for each_pet in shop[:pets]
     breed_total << each_pet if each_pet[:breed] == breed_search
-    end
+  end
   return breed_total
 end
 
 def find_pet_by_name(shop, expected_name)
-result = nil
-    for pet in shop[:pets]
-      if pet[:name] == expected_name
-        result = pet
-      end
+  result = nil
+  for pet in shop[:pets]
+    if pet[:name] == expected_name
+      result = pet
     end
+  end
   return result
 end
 
@@ -70,22 +70,20 @@ def add_pet_to_customer(customer_info, pet)
   return customer_info[:pets].push(pet)
 end
 
-def customer_can_afford_pet(customer_info, pet)
+def customer_can_afford_pet?(customer_info, pet)
   return false if pet == nil
   customer_info[:cash] - pet[:price] > 0 ? true : false
 end
 
 def sell_pet_to_customer(shop, pet_to_sell, customer_buying)
-  affordability = customer_can_afford_pet(customer_buying, pet_to_sell)
+  affordability = customer_can_afford_pet?(customer_buying, pet_to_sell)
 
-  if affordability == true
-    customer_buying[:pets].push(pet_to_sell)
-    shop[:admin][:pets_sold] += 1
-    shop[:admin][:total_cash] += pet_to_sell[:price]
-  else
-    customer_buying[:pets]
-    shop[:admin][:pets_sold]#pets_sold(shop)
-    shop[:admin][:total_cash]#total_cash(shop)
+  if affordability
+    add_pet_to_customer(customer_buying, pet_to_sell)
+    increase_pets_sold(shop, 1)
+    add_or_remove_cash(shop, pet_to_sell[:price])
   end
 end
+
+
 #I was returning NoMethodError: undefined method `[]' for nil:NilClass for ages and knew that I was trying to call an array method on nil, but couldn't see where it was happening. I realised it's in the customer_can_afford_pet function - if you pass in a pet == nil, then the function didn't account for this and it was trying to call pet[:price], which is obviously not compatible if pet == nil.
